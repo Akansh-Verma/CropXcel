edb9cbeed59cfb62ebe248408273877daee684c7
AkanshVerma <vermaakansh7@gmail.com>
2023-03-12 22:31:38 +0530

    Initial boilerplate code

---
+++ cropxcel-web/src/main/java/com/cropxcel/cropxcelweb/CropxcelWebApplication.java
@@ -0,0 +1,13 @@
+package com.cropxcel.cropxcelweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CropxcelWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CropxcelWebApplication.class, args);
+	}
+
+}
---
+++ cropxcel-web/src/test/java/com/cropxcel/cropxcelweb/CropxcelWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cropxcel.cropxcelweb;
+
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+// class CropxcelWebApplicationTests {
+
+// @Test
+// void contextLoads() {
+// }
+
+// }
